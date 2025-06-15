fb9304584fdc29eced33ae5128e26bec741eb82f
Sahil Goes <sahilgoes87@gmail.com>
2025-06-15 22:33:51 +0530

    gfg potd 15 Jun 2025 (Added optimized approach)

--- smallest_divisor.cpp
+++ smallest_divisor.cpp
@@ -17,3 +17,32 @@
         }
         return ans;
     }
+
+//Optimized Approach: Using Binary Search to find which ans offers more possibility to be the correct ans
+ int computeSum(vector<int>& arr, int divisor){
+        int total = 0;
+        for (int num : arr) {
+            total += (num + divisor - 1) / divisor;  // Efficient ceil
+        }
+        return total;
+    }
+  
+    int smallestDivisor(vector<int>& arr, int k) {
+        int low = 1;
+        int high = *max_element(arr.begin(), arr.end());
+        int ans = high;
+    
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int currSum = computeSum(arr, mid);
+    
+            if (currSum <= k) {
+                ans = mid;           // Candidate found, try smaller
+                high = mid - 1;
+            } else {
+                low = mid + 1;       // Too small, try larger
+            }
+        }
+    
+        return ans;
+    }
